e464650e0c36dd017f08ac6af7368a38e1fd579a
pxcxx <ramoscidjuanfrancisco@gmail.com>
2026-06-30 22:54:56 -0600

    ya que quede porfa

--- src/main/java/com/exam/planetas/config/CorsConfig.java
+++ src/main/java/com/exam/planetas/config/CorsConfig.java
@@ -1,5 +1,3 @@
-package com.exam.planetas.config;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,13 +8,11 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
+                .allowedOriginPatterns(
                         "https://planetas-frontend.onrender.com",
-                        "http://localhost:3000",
-                        "http://localhost:4200",
-                        "http://127.0.0.1:3000",
-                        "http://127.0.0.1:4200",
-"*"                )
+                        "http://localhost:*",
+                        "http://127.0.0.1:*"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("*")
